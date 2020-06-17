7e71243093cd25a8224dfa05d64773a1cc4903e8
wnsduq6229 <57088470+wnsduq6229@users.noreply.github.com>
2020-06-17 23:31:22 +0900

    모든 창 연결, 화면 수정
    2020/06/17
    description창 시작하기 나가기 위치 수정
    
    2020/06/17 20:29
    math게임에서 토끼그림 삭제
    
    2020/06/17/ 21:28
    각 게임의 result창에서 확인을 누를 시에 게임선택창으로 이동하도록 설정
    
    2020/06/17/ 23:31
    Title2_2화면 삭제후 다른 화면들 연결

--- app/src/main/java/com/example/a10/androidpoject/Title1_Activity.java
+++ app/src/main/java/com/example/a10/androidpoject/Title1_Activity.java
@@ -27,7 +27,7 @@ public class Title1_Activity extends AppCompatActivity {
              @Override
              public void onClick(View v) {
                  Intent intent = new Intent(
-                         getApplicationContext(),Title2_Activity.class);//현재 화면의 제어권자,다음 넘어갈 클래스 지정
+                         getApplicationContext(),Title2_2Activity.class);//현재 화면의 제어권자,다음 넘어갈 클래스 지정
                  startActivity(intent);//다음화면으로 넘어간다.
              }
          });
